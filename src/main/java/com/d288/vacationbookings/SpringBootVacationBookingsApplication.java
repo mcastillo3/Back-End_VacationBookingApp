8b4d76ce994af2e8afd2b7cc7769728dd84af7b6
Mauro Castillo <mauro.m.castillo3@gmail.com>
2024-04-10 18:40:03 -0700

    Initiated project with Springboot dependencies

---
+++ src/main/java/com/d288/vacationbookings/SpringBootVacationBookingsApplication.java
@@ -0,0 +1,13 @@
+package com.d288.vacationbookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVacationBookingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVacationBookingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d288/vacationbookings/SpringBootVacationBookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d288.vacationbookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVacationBookingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
